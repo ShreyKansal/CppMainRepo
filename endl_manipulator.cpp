73b6a794b63b1c8836bc43fcfc0073814f8b743d
ShreyKansal <skansal801@gmail.com>
2022-11-07 12:03:44 +0530

    Endl Manipulator

---
+++ endl_manipulator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  cout << "How are you?"<< endl;
+ 
+// We can also use \n to change into new line 
+
+  cout << "Hello World!\n";
+  cout << "How are you?"; 
+   return 0;
+}
