b3d354f2c70dda0c630c7f63245518c42ef5d243
ShreyKansal <skansal801@gmail.com>
2023-01-04 23:32:23 +0530

    Strings

---
+++ Strings/string.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string str = "Hello guys!";
+    /*Another way of storing can be: 
+      char str[5]= "string"
+      char str[]= {'s','t','r','i','n','g'} */
+    cout << str;
+}
