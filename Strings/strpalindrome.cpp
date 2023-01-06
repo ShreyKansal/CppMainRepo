e73643e4d5068d62a2e347e159e7e721d10c65fa
ShreyKansal <skansal801@gmail.com>
2023-01-06 14:19:32 +0530

    Strings

---
+++ Strings/strpalindrome.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+#include <string> 
+using namespace std;
+int main() {
+    string str = "abba";
+    int l = 0; //initialise leftmost index position
+    int r = str.size()-1; //initialise rightmost index position
+    while (r>l) {
+        if (str[l++] != str[r--]) { //comparing left and right index values from string
+            cout << "String is not a palindrome" <<endl;
+            return 0;         }
+        }
+        cout << "String is a palindrome" << endl;
+    return 0;
+}
