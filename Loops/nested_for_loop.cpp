00011a4ab7a7916bae80bab5149d2bca720cf813
ShreyKansal <skansal801@gmail.com>
2023-01-08 20:39:23 +0530

    Loops

---
+++ Loops/nested_for_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std; 
+int main() {
+    int n; 
+    int m;
+    for (m=10; m<=10; m++) {
+    for (n =1 ; n<=10; n++) {
+    cout << m << "*" << n<< "=" << (m*n) << endl;
+    }
+    }
+    return 0;
+}
+
+//the above program is an example of nested-for loop. 
